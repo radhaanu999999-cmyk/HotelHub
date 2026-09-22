686280cfac447d9e1feab5a89138b60dde975800
Nuvvula Anuradha <radhaanu999999@gmail.com>
2026-09-22 10:47:12 +0530

    Connect hotel search to Render

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ if (searchButton) {
         try {
 
             const response = await fetch(
-                "http://localhost:5000/api/hotels"
+              "https://hotelhub-p5vf.onrender.com/api/hotels"
             );
 
 
